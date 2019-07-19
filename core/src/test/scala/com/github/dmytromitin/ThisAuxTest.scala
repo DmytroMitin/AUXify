78bb421ae9aa6afa3c1f51d171772c6d17e00e8c
DmytroMitin <dmitin3@gmail.com>
2019-07-19 16:50:12 +0300

    fix #2

---
+++ core/src/test/scala/com/github/dmytromitin/ThisAuxTest.scala
@@ -0,0 +1,49 @@
+package com.github.dmytromitin
+
+import org.scalatest._
+
+class ThisAuxTest extends FlatSpec with Matchers {
+  sealed trait Nat
+  case object _0 extends Nat
+  type _0 = _0.type
+  case class Succ[N <: Nat](n: N) extends Nat
+
+  type _1 = Succ[_0]
+  type _2 = Succ[_1]
+  type _3 = Succ[_2]
+  type _4 = Succ[_3]
+  type _5 = Succ[_4]
+
+  val _1: _1 = Succ(_0)
+  val _2: _2 = Succ(_1)
+  val _3: _3 = Succ(_2)
+  val _4: _4 = Succ(_3)
+  val _5: _5 = Succ(_4)
+
+  @This(lowerBound = false, fBound = false) @Aux
+  trait Add[N <: Nat, M <: Nat] { /*self =>*/
+//    type This /*>: this.type*/ <: Add[N, M] /*{ type This = self.This }*/
+    type Out <: Nat
+    def apply(n: N, m: M): Out
+  }
+
+  object Add {
+//    type Aux[N <: Nat, M <: Nat, This0 <: Add[N, M], Out0 <: Nat] = Add[N, M] { type This = This0; type Out = Out0 }
+    def instance[N <: Nat, M <: Nat, This0 <: Add[N, M], Out0 <: Nat](f: (N, M) => Out0): Aux[N, M, This0, Out0] = new Add[N, M] {
+      override type This = This0
+      override type Out = Out0
+      override def apply(n: N, m: M): Out = f(n, m)
+    }
+
+    implicit def zeroAdd[M <: Nat]: Aux[_0, M, Add[_0, M], M] = instance((_, m) => m)
+    implicit def succAdd[N <: Nat, M <: Nat, N_addM <: Nat](implicit add: Aux[N, M, Add[N, M], N_addM]): Aux[Succ[N], M, Add[Succ[N], M], Succ[N_addM]] =
+      instance((succN, m) => Succ(add(succN.n, m)))
+  }
+
+  implicitly[Add.Aux[_2, _3, Add[_2, _3], _5]]
+
+  "2 + 3" should "be 5" in {
+    implicitly[Add[_2, _3]].apply(_2, _3) should be (_5)
+  }
+
+}
--- macros/src/main/scala/com/github/dmytromitin/This.scala
+++ macros/src/main/scala/com/github/dmytromitin/This.scala
@@ -4,13 +4,26 @@ import scala.annotation.StaticAnnotation
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
-class This extends StaticAnnotation {
+class This(lowerBound: Boolean = true, fBound: Boolean = true) extends StaticAnnotation {
   def macroTransform(annottees: Any*): Any = macro thisMacro.impl
 }
 
 object thisMacro {
   def impl(c: blackbox.Context)(annottees: c.Tree*): c.Tree = {
     import c.universe._
+
+    val (isLowerBoundOn, isFBoundOn) = c.prefix.tree match {
+      case q"new This(lowerBound = ${lb: Boolean}, fBound     = ${fb: Boolean})" => (lb,   fb)
+      case q"new This(fBound     = ${fb: Boolean}, lowerBound = ${lb: Boolean})" => (lb,   fb)
+      case q"new This(fBound     = ${fb: Boolean}                             )" => (true, fb)
+      case q"new This(lowerBound = ${lb: Boolean}                             )" => (lb,   true)
+      case q"new This(lowerBound = ${lb: Boolean},              ${fb: Boolean})" => (lb,   fb)
+      case q"new This(             ${lb: Boolean}, fBound     = ${fb: Boolean})" => (lb,   fb)
+      case q"new This(             ${lb: Boolean},              ${fb: Boolean})" => (lb,   fb)
+      case q"new This(             ${lb: Boolean}                             )" => (lb,   true)
+      case q"new This(                                                        )" => (true, true)
+      case _ => c.abort(c.enclosingPosition, "not boolean literal")
+    }
 
     def modifyTparams(tparams: Seq[Tree]): Seq[Tree] = tparams.map {
       case q"$_ type $name[..$tparams] >: $_ <: $_" => tq"$name"
@@ -26,14 +39,17 @@ object thisMacro {
         val self2 = self1 match {
           case q"$_ val $selfName: $_ = $_" => selfName
         }
+        val lowerBound = if (isLowerBoundOn) tq"this.type" else tq"_root_.scala.Nothing"
+        val fBound = if (isFBoundOn) Seq(q"type This = $self2.This") else Seq[Tree]()
         q"""
             $mods trait $tpname[..$tparams] extends { ..$earlydefns } with ..$parents { $self1 =>
-              type This >: this.type <: $tpname[..${modifyTparams(tparams)}] { type This = $self2.This }
+              type This >: $lowerBound <: $tpname[..${modifyTparams(tparams)}] { ..$fBound }
               ..$stats
             }
 
             ..$tail
           """
+
       case q"$mods class $tpname[..$tparams] $ctorMods(...$paramss) extends { ..$earlydefns } with ..$parents { $self => ..$stats }" :: tail =>
         q"""
             $mods class $tpname[..$tparams] $ctorMods(...$paramss) extends { ..$earlydefns } with ..$parents { $self =>
