b207cf8fbd826031a58f6522673b6fdfc5d0ad46
DmytroMitin <dmitin3@gmail.com>
2019-07-22 21:28:01 +0300

    refactoring

--- macros/src/main/scala/com/github/dmytromitin/instance.scala
+++ macros/src/main/scala/com/github/dmytromitin/instance.scala
@@ -18,53 +18,66 @@ class InstanceMacro(val c: whitebox.Context) extends Helpers {
     def createFunctionType(paramss: Seq[Seq[Tree]], tpt: Tree): Tree =
       paramss.foldRight(tpt: Tree)((params: Seq[Tree], acc: Tree) => tq"(..$params) => $acc")
 
+    def modifyTyp(mods: Modifiers, name: TypeName, tparams: Seq[TypeDef], low: Tree, high: Tree, typeNameMap: Map[TypeName, TypeName]): (Option[TypeDef], Option[TypeDef], Option[Tree], Tree) = {
+      val name0 = typeNameMap(name)
+      val modifiedTparams = modifyTparams(tparams)
+      def mkStat(ms: Modifiers) = q"$ms type $name[..${modifiedTparams._1}] = $name0[..${modifiedTparams._2}]"
+      (
+        Some(q"${mods & ~Flag.DEFERRED | Flag.PARAM} type $name0[..$tparams] >: $low <: $high"),
+        Some(mkStat(mods & ~Flag.DEFERRED)),
+        None,
+        mkStat(mods & ~Flag.DEFERRED | Flag.OVERRIDE)
+      )
+    }
+
+    def modifyDef(mods: Modifiers, tname: TermName, paramss: Seq[Seq[Tree]], typeNameMap: Map[TypeName, TypeName], tpt: Tree): (Option[TypeDef], Option[TypeDef], Option[Tree], Tree) = {
+      val f = TermName(c.freshName("f"))
+      val (paramTypess, paramNamess) = modifyParamss(paramss)
+      val domain = paramTypess.map(_.map(modifyType(_, typeNameMap)))
+      val codomain = modifyType(tpt, typeNameMap)
+      val functionType = createFunctionType(domain, codomain)
+      (
+        None,
+        None,
+        Some(q"${mods & ~Flag.DEFERRED | Flag.PARAM} val $f: $functionType = $EmptyTree"),
+        q"${mods & ~Flag.DEFERRED | Flag.OVERRIDE} def $tname[..${Seq[Tree]()}](...$paramss): $tpt = $f(...$paramNamess)"
+      )
+    }
+
+    def modifyVal(mods: Modifiers, tname: TermName, tpt: Tree, typeNameMap: Map[TypeName, TypeName]): (Option[TypeDef], Option[TypeDef], Option[Tree], Tree) = {
+      val f = TermName(c.freshName("f"))
+      val codomain = modifyType(tpt, typeNameMap)
+      (
+        None,
+        None,
+        Some(q"${mods & ~Flag.DEFERRED | Flag.PARAM | Flag.BYNAMEPARAM} val $f: $codomain = $EmptyTree"),
+        q"${mods & ~Flag.DEFERRED | Flag.OVERRIDE} val $tname: $tpt = $f"
+      )
+    }
+
+    def modifyVar(mods: Modifiers, tname: TermName, tpt: Tree, typeNameMap: Map[TypeName, TypeName]): (Option[TypeDef], Option[TypeDef], Option[Tree], Tree) = {
+      val f = TermName(c.freshName("f"))
+      val codomain = modifyType(tpt, typeNameMap)
+      (
+        None,
+        None,
+        Some(q"${mods & ~Flag.DEFERRED | Flag.PARAM | Flag.BYNAMEPARAM} val $f: $codomain = $EmptyTree"),
+        q"${mods & ~Flag.DEFERRED | Flag.OVERRIDE} var $tname: $tpt = $f"
+      )
+    }
+
     def modifyStats(stats: Seq[Tree], typeNameMap: Map[TypeName, TypeName]): (Seq[TypeDef], Seq[TypeDef], Seq[Tree], Seq[Tree]) = {
       val res: Seq[(Option[TypeDef], Option[TypeDef], Option[Tree], Tree)] = stats.map {
         case q"${mods: Modifiers} type $name[..$tparams] >: $low <: $high" =>
-          val name0 = typeNameMap(name)
-          val modifiedTparams = modifyTparams(tparams)
-          def mkStat(ms: Modifiers) = q"$ms type $name[..${modifiedTparams._1}] = $name0[..${modifiedTparams._2}]"
-          (
-            Some(q"${mods & ~Flag.DEFERRED | Flag.PARAM} type $name0[..$tparams] >: $low <: $high"),
-            Some(mkStat(mods & ~Flag.DEFERRED)),
-            None,
-            mkStat(mods & ~Flag.DEFERRED | Flag.OVERRIDE)
-          )
+          modifyTyp(mods, name, tparams, low, high, typeNameMap)
 
         case q"${mods: Modifiers} def $tname[..$tparams](...$paramss): $tpt = ${`EmptyTree`}" =>
-          if (tparams.isEmpty) {
-            val f = TermName(c.freshName("f"))
-            val (paramTypess, paramNamess) = modifyParamss(paramss)
-            val domain = paramTypess.map(_.map(modifyType(_, typeNameMap)))
-            val codomain = modifyType(tpt, typeNameMap)
-            val functionType = createFunctionType(domain, codomain)
-            (
-              None,
-              None,
-              Some(q"${mods & ~Flag.DEFERRED | Flag.PARAM} val $f: $functionType = $EmptyTree"),
-              q"${mods & ~Flag.DEFERRED | Flag.OVERRIDE} def $tname[..${Seq[Tree]()}](...$paramss): $tpt = $f(...$paramNamess)"
-            )
-          } else c.abort(c.enclosingPosition, "polymorphic method")
+          if (tparams.isEmpty) modifyDef(mods, tname, paramss, typeNameMap, tpt)
+          else c.abort(c.enclosingPosition, "polymorphic method")
 
-        case q"$mods val $tname: $tpt = ${`EmptyTree`}" =>
-          val f = TermName(c.freshName("f"))
-          val codomain = modifyType(tpt, typeNameMap)
-          (
-            None,
-            None,
-            Some(q"${mods & ~Flag.DEFERRED | Flag.PARAM | Flag.BYNAMEPARAM} val $f: $codomain = $EmptyTree"),
-            q"${mods & ~Flag.DEFERRED | Flag.OVERRIDE} val $tname: $tpt = $f"
-          )
+        case q"$mods val $tname: $tpt = ${`EmptyTree`}" => modifyVal(mods, tname, tpt, typeNameMap)
 
-        case q"$mods var $tname: $tpt = ${`EmptyTree`}" =>
-          val f = TermName(c.freshName("f"))
-          val codomain = modifyType(tpt, typeNameMap)
-          (
-            None,
-            None,
-            Some(q"${mods & ~Flag.DEFERRED | Flag.PARAM | Flag.BYNAMEPARAM} val $f: $codomain = $EmptyTree"),
-            q"${mods & ~Flag.DEFERRED | Flag.OVERRIDE} var $tname: $tpt = $f"
-          )
+        case q"$mods var $tname: $tpt = ${`EmptyTree`}" => modifyVar(mods, tname, tpt, typeNameMap)
 
         case stat => (None, None, None, stat)
       }
--- macros/src/main/scala/com/github/dmytromitin/syntax.scala
+++ macros/src/main/scala/com/github/dmytromitin/syntax.scala
@@ -17,47 +17,52 @@ class SyntaxMacro(val c: whitebox.Context) extends Helpers {
   import c.universe._
 
   def impl(annottees: Tree*): Tree = {
-    def modifyStat(tparams: Seq[TypeDef], tpname: TypeName, typeNameSet: Set[TypeName]): PartialFunction[Tree, Tree] = {
+
+    def typeDependents(typ: Tree): Set[TypeName] = {
+      val res = mutable.Set[TypeName]()
+
+      val traverser = new Traverser {
+        override def traverse(tree: Tree): Unit = tree match {
+          case tq"${name: TypeName}" => res += name
+          case _ => super.traverse(tree)
+        }
+      }
+
+      traverser.traverse(typ)
+
+      res.toSet
+    }
+
+    def firstParamTypeDependsOn(tparam: Tree, set: Set[TypeName]): Boolean = tparam match {
+      case q"$mods type $tpname[..$tparams] = $tpt" => set.contains(tpname)
+    }
+
+    def mkRHS(tname: TermName, methodTparams: Seq[Tree], paramss: Seq[Seq[Tree]], inst: TermName): Tree = {
+      val methodTparams1 = modifyTparams(methodTparams)
+      val paramNamess = modifyParamss(paramss)._2
+      q"$inst.$tname[..${methodTparams1._2}](...$paramNamess)"
+    }
+
+    def createImplicitClass(tparams: Seq[TypeDef], tpname: TypeName, typeNameSet: Set[TypeName]): PartialFunction[Tree, Tree] = {
       case q"${mods: Modifiers} def $tname[..$methodTparams](...$paramss): $tpt = ${`EmptyTree`}" if paramss.nonEmpty && paramss.head.nonEmpty =>
-        val inst = TermName(c.freshName("inst"))
         val ops = TypeName(c.freshName("Ops"))
+        val inst = TermName(c.freshName("inst"))
+        val tpt1 = modifyType(tpt, typeNameSet, inst)
 
         val tparams1 = modifyTparams(tparams)
-        val methodTparams1 = modifyTparams(methodTparams)
-        val paramNamess = modifyParamss(paramss)._2
-        val tpt1 = modifyType(tpt, typeNameSet, inst)
         val implct = q"implicit val $inst: $tpname[..${tparams1._2}]"
-
-        val allTparams: Seq[Tree] = tparams1._1 ++ methodTparams
-        val firstParam: Tree = paramss.head.head
-        val restParamss: Seq[Seq[Tree]] = paramss.head.tail +: paramss.tail
-        val firstParamType: Tree = modifyParam(firstParam)._1
-
-        val firstParamTypeDependents: Set[TypeName] = {
-          val res = mutable.Set[TypeName]()
-
-          val traverser = new Traverser {
-            override def traverse(tree: Tree): Unit = tree match {
-              case tq"${name: TypeName}" => res += name
-              case _ => super.traverse(tree)
-            }
-          }
-
-          traverser.traverse(firstParamType)
-
-          res.toSet
-        }
-
-        def firstParamTypeDependsOn(tparam: Tree): Boolean = tparam match {
-          case q"$mods type $tpname[..$tparams] = $tpt" => firstParamTypeDependents.contains(tpname)
-        }
-
-        val (firstTparams: Seq[Tree], restTparams: Seq[Tree]) = allTparams.partition(firstParamTypeDependsOn)
+        val allTparams = tparams1._1 ++ methodTparams
+        val (firstParam, restParamss) = (paramss.head.head, paramss.head.tail +: paramss.tail)
+        val firstParamType = modifyParam(firstParam)._1
+        val firstParamTypeDependents = typeDependents(firstParamType)
+        val (firstTparams, restTparams) = allTparams.partition(firstParamTypeDependsOn(_, firstParamTypeDependents))
+        val restParamssWithImplct = addImplicitToParamss(restParamss, implct)
+        val rhs = mkRHS(tname, methodTparams, paramss, inst)
 
         q"""
            implicit class $ops[..$firstTparams]($firstParam) {
              ..${Seq(
-               q"${mods & ~Flag.DEFERRED} def $tname[..$restTparams](...${addImplicitToParamss(restParamss, implct)}): $tpt1 = $inst.$tname[..${methodTparams1._2}](...$paramNamess)"
+               q"${mods & ~Flag.DEFERRED} def $tname[..$restTparams](...$restParamssWithImplct): $tpt1 = $rhs"
              )}
            }
          """
@@ -66,7 +71,7 @@ class SyntaxMacro(val c: whitebox.Context) extends Helpers {
 
     def createExtensionMethods(tparams: Seq[TypeDef], tpname: TypeName, stats: Seq[Tree]): Seq[Tree] = {
       val typeNameSet = createTypeNameSet(stats)
-      stats.collect(modifyStat(tparams, tpname, typeNameSet))
+      stats.collect(createImplicitClass(tparams, tpname, typeNameSet))
     }
 
     modifyAnnottees(annottees, (tparams, tpname, stats) => Seq(
