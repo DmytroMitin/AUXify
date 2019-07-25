300639ffa64816ad04c8c6658b2febcf1b2e223c
DmytroMitin <dmitin3@gmail.com>
2019-07-25 11:38:21 +0300

    temporaly ignore test

--- meta-tests/src/test/scala/com/github/dmytromitin/auxify/meta/RuleSuite.scala
+++ meta-tests/src/test/scala/com/github/dmytromitin/auxify/meta/RuleSuite.scala
@@ -3,7 +3,7 @@ package com.github.dmytromitin.auxify.meta
 import org.scalatest.Ignore
 import scalafix.testkit.SemanticRuleSuite
 
-@Ignore
+@Ignore // TODO #11
 class RuleSuite extends SemanticRuleSuite() {
 
   runAllTests()
