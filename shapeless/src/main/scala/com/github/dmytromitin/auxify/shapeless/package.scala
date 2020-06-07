06528e9b1765604306f6e0d6b7fcf2154e7d8630
DmytroMitin <dmitin3@gmail.com>
2020-06-07 18:44:57 +0300

    Replace inference <: Singleton (2.13) with macro + .narrow

--- shapeless/src/main/scala/com/github/dmytromitin/auxify/shapeless/package.scala
+++ shapeless/src/main/scala/com/github/dmytromitin/auxify/shapeless/package.scala
@@ -6,12 +6,21 @@ import scala.reflect.macros.whitebox
 package object shapeless {
   import scala.language.experimental.macros
 
-  def stringToSymbol[S <: String with Singleton, S1 <: Symbol](s: S)(implicit
-                                                                     sts: StringToSymbol.Aux[S, S1],
-                                                                     valueOf: Witness.Aux[S1]): S1 = valueOf.value
+  def stringToSymbolHlp[S <: String with Singleton, S1 <: Symbol](s: S)(implicit
+                                                                        sts: StringToSymbol.Aux[S, S1],
+                                                                        witness: Witness.Aux[S1]): S1 = witness.value
+  def stringToSymbol(s: String): Symbol = macro stringToSymbolImpl
+  def stringToSymbolImpl(c: whitebox.Context)(s: c.Tree): c.Tree = {
+    import c.universe._
+    q"""
+      import _root_.shapeless.syntax.singleton._
+      _root_.com.github.dmytromitin.auxify.shapeless.`package`.stringToSymbolHlp($s.narrow)
+    """
+  }
+
   def symbolToStringHlp[S <: Symbol, S1 <: String with Singleton](s: S)(implicit
                                                                         sts: SymbolToString.Aux[S, S1],
-                                                                        valueOf: /*ValueOf*/Witness.Aux[S1]): S1 = valueOf.value
+                                                                        witness: Witness.Aux[S1]): S1 = witness.value
   def symbolToString(s: Symbol): String = macro symbolToStringImpl
   def symbolToStringImpl(c: whitebox.Context)(s: c.Tree): c.Tree = {
     import c.universe._
