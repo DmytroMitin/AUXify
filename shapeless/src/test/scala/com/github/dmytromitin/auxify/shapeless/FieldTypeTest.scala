ca6a2494355507a69abe9def561b501b04b65b42
DmytroMitin <dmitin3@gmail.com>
2020-06-29 17:21:33 +0300

    Add FieldType test

--- shapeless/src/test/scala/com/github/dmytromitin/auxify/shapeless/FieldTypeTest.scala
+++ shapeless/src/test/scala/com/github/dmytromitin/auxify/shapeless/FieldTypeTest.scala
@@ -4,8 +4,8 @@ import shapeless.Witness
 import shapeless.labelled.FieldType
 import shapeless.syntax.singleton._
 
-//https://stackoverflow.com/questions/50526509/using-string-instead-of-symbol-in-labelledgeneric-etc
+//FIXME #36
 class FieldTypeTest {
-  def syFldToStrFld[St <: String, Sy <: Symbol, T](syFld : FieldType[Sy,T])(implicit syWit : Witness.Aux[Sy]) : FieldType[St,T] =
-    (syWit.value.name : String) ->> (syFld : T)
+//  def syFldToStrFld[St <: String, Sy <: Symbol, T](syFld : FieldType[Sy,T])(implicit syWit : Witness.Aux[Sy]) : FieldType[St,T] =
+//    (syWit.value.name : String) ->> (syFld : T)
 }
